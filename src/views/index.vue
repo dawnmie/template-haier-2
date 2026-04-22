a799ba55bae6a4bef8a762afdff9579eb8f49561
崔久代 <cuijiudai@qq.com>
2026-04-22 15:24:28 +0800

    aibot-1208 feat 更换首页欢迎语言

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="welcome-wrap">
-    <p class="welcome-text">欢迎使用轻应用</p>
+    <h3>需求梳理进行中</h3>
+    <p class="welcome-text">完成全部需求问答后，将自动为您生成专属轻应用</p>
   </div>
 </template>
 
@@ -10,14 +11,16 @@
 .welcome-wrap {
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   margin: 0;
   background: #fafafa;
+  text-align: center;
 }
 
 .welcome-text {
-  font-size: 18px;
+  font-size: 16px;
   color: rgba(0, 0, 0, 0.65);
   margin: 0;
 }
